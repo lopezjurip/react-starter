a8f19b8c254df0e6cec072226e73a315a38ad9e9
Patricio López <lopezjuripatricio@gmail.com>
2016-04-02 11:25:53 -0300

    add state and click to helloworld component

--- src/components/helloworld.js
+++ src/components/helloworld.js
@@ -2,13 +2,30 @@ import React, { Component, StyleSheet } from 'react'
 
 
 export default class HelloWorld extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      on: true,
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div onClick={this.handleClick.bind(this)}>
         <h1>Hello, world.</h1>
-        <h2 style={styles.detail}>Subtitle: {this.props.subtitle}</h2>
+        <h2 style={styles.detail}>
+          Subtitle: {this.props.subtitle}
+        </h2>
+        <p>
+          State: {this.state.on ? 'on' : 'off'} (click on me)
+        </p>
       </div>
     )
+  }
+
+  handleClick() {
+    this.setState({ on: !this.state.on })
   }
 }
 
